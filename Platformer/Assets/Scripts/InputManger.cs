49eb9e26428d30bba42b9faa14bbd627a6c6b6dc
benforcapita <benforcapita@gmail.com>
2016-08-30 09:50:55 +0300

    First Commite
    Added Unity Project

---
+++ Platformer/Assets/Scripts/InputManger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum Buttons
+{
+    Right,
+    Left,
+    Jump
+}
+[System.Serializable]
+public class inputAxisState
+{
+    public string axisName;
+    public float offValue;
+    public Buttons buttons;
+
+}
+
+public class InputManger : MonoBehaviour {
+
+    public inputAxisState[] inputs;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
